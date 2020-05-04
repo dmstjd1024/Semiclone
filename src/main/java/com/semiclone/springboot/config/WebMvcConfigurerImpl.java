2d3bcb375121fcb8b203da8028a662d1f611a0ab
rogunte <xsypqj@gmail.com>
2020-05-04 17:24:19 +0900

    Update CORS

--- src/main/java/com/semiclone/springboot/config/WebMvcConfigurerImpl.java
+++ src/main/java/com/semiclone/springboot/config/WebMvcConfigurerImpl.java
@@ -1,6 +1,7 @@
 package com.semiclone.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,11 +10,13 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
-                .allowedMethods("PATCH")
-                .allowedMethods("GET")
-                .allowedMethods("POST")
-                .allowedMethods("DELETE")
-                .allowedMethods("PUT")
+                .allowedMethods(
+                    HttpMethod.GET.name(),
+                    HttpMethod.HEAD.name(),
+                    HttpMethod.POST.name(),
+                    HttpMethod.PUT.name(),
+                    HttpMethod.DELETE.name(),
+                    HttpMethod.PATCH.name())
                 .allowedOrigins("http://localhost:3000"); //허용할 오리진들
     }
 }
