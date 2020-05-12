3b62488d81ddc5da7569ce50ae1d8b8cbf5526a3
dmstjd1024 <dmstjd1024@naver.com>
2020-05-12 16:18:23 +0900

    결제 template 추가

--- src/main/java/com/semiclone/springboot/web/GiftShopController.java
+++ src/main/java/com/semiclone/springboot/web/GiftShopController.java
@@ -3,12 +3,16 @@ package com.semiclone.springboot.web;
 import com.semiclone.springboot.domain.product.Product;
 import com.semiclone.springboot.domain.product.ProductRepository;
 import com.semiclone.springboot.service.ProductService;
+import com.semiclone.springboot.service.PurchaseService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/giftshop")
@@ -17,6 +21,8 @@ public class GiftShopController {
 
     private final ProductRepository productRepository;
     private final ProductService productService;
+
+    private final PurchaseService purchaseService;
 
     @GetMapping("") // 기프트샵 메인
     private String index(Model model){
@@ -54,19 +60,18 @@ public class GiftShopController {
         return "giftshop/product-detail";
     }
 
-    @GetMapping("/purchase-confirm")
-    public String purchaseConfirm(Model model){
+    @GetMapping("/payment-confirm")
+    public String paymentConfirm(Model model){
 
         return "";
     }
 
 
+    @ApiOperation(value = "결제 :: 추가완료 시 1 return / 실패 시 0 return",
+            notes = "Server로 보낼 Data {\"imp_uid\":1234}")
+    @PostMapping("/payment")
+    public Map<String, Object> payment(@RequestBody Map<String, Object> purchaseMap, Principal principal) throws Exception{
 
-
-
-
-/*    @PostMapping("/purchase-confirm") //결제하기 클릭 시
-    private String payment(Model model, ){
-
-    }*/
+        return purchaseService.giftshopPurchase(purchaseMap, principal.getName());
+    }
 }
