c289fbc2ba3158331d3b1b4352a43ccb49323e1c
rogunte <xsypqj@gmail.com>
2020-05-25 19:07:17 +0900

    build.gradle Test코드 삭제 & Test.java 실패코드 삭제

--- src/test/java/com/semiclone/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/semiclone/springboot/SpringbootApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootApplicationTests {
+public class SpringbootApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
