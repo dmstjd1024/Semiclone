e0371465ce9db50d9671fc4a94e2dcd149fa15b9
rogunte <xsypqj@gmail.com>
2020-05-29 09:27:11 +0900

    Refactoring showTimes/timetables

--- src/main/java/com/semiclone/springboot/service/timetable/TimeTableServiceImpl.java
+++ src/main/java/com/semiclone/springboot/service/timetable/TimeTableServiceImpl.java
@@ -8,7 +8,9 @@ import java.util.Map;
 import com.google.gson.Gson;
 import com.semiclone.springboot.domain.cinema.CinemaRepository;
 import com.semiclone.springboot.domain.movie.Movie;
+import com.semiclone.springboot.domain.movie.MovieInfo;
 import com.semiclone.springboot.domain.movie.MovieRepository;
+import com.semiclone.springboot.domain.screen.Screen;
 import com.semiclone.springboot.domain.screen.ScreenRepository;
 import com.semiclone.springboot.domain.timetable.TimeTable;
 import com.semiclone.springboot.domain.timetable.TimeTableRepository;
@@ -30,6 +32,8 @@ public class TimeTableServiceImpl implements TimeTableService {
     private final ScreenRepository screenRepository;
     private final TimeTableRepository timeTableRepository;
     private final TicketService ticketService;
+    int sum = 0;
+    int say = 0;
 
     /* 극장 리스트 */
     public Map<String, Object> getCinemas() throws Exception {
@@ -55,32 +59,44 @@ public class TimeTableServiceImpl implements TimeTableService {
 
     /* 극장별 상영시간표 */
     public Map<String, Object> getTimeTablesByCinemaId(Long cinemaId, Long date) throws Exception {
-        
+        Long start = System.currentTimeMillis();
         date = (date == 123890) ? null : date;    // 테스트용 null값
         date = (date == null || date == 0) ? timeTableRepository.findFirstByCinemaIdOrderByDate(cinemaId).getDate() : date ;
 
         /* 극장별 날짜,영화에 해당되는 상영 시간표 */
         List<Map<String, Object>> timeTablesList = new ArrayList<Map<String, Object>>();
-        List<Long> movieIdsList = timeTableRepository.findMovieIdByCinemaIdAndDate(cinemaId, date);
-        for(Long movieId : movieIdsList){
-            List<Long> screendIdsList = timeTableRepository.findScreenIdByCinemaIdAndDateAndMovieId(cinemaId, date, movieId);
-            int screenIdSize = screendIdsList.size(); 
+        List<MovieInfo> movieInfosList = timeTableRepository.findMovieInfoByCinemaIdAndDate(cinemaId, date);
+        for(MovieInfo movie : movieInfosList){
+            Long movieId = movie.getId();
+            List<Screen> screenList = timeTableRepository.findScreenByCinemaIdAndDateAndMovieId(cinemaId, date, movieId);
+            int screenIdSize = screenList.size(); 
             Map[] screensList = new HashMap[screenIdSize];
             for(int j=0; j<screenIdSize; j++){
-                Long screenId = screendIdsList.get(j);
                 Map<String, Object> screensMap = new HashMap<String, Object>();
-                screensMap.put("screen", screenRepository.findOneById(screenId));
-                screensMap.put("timeTables", timeTableRepository.findTimeTableByMovieIdAndScreenIdAndDate(movieId, screenId, date));
+                Screen screen = screenList.get(j);
+                screensMap.put("screen", screen);
+                screensMap.put("timeTables", timeTableRepository.findTimeTableByMovieIdAndScreenIdAndDate(movieId, screen.getId(), date));
                 screensList[j] = screensMap;
             }
             Map<String, Object> moviesMap = new HashMap<String, Object>();
-            moviesMap.put("movie", new MovieInfoDto(movieRepository.findOneById(movieId)));
+            moviesMap.put("movie", new MovieInfoDto(movie));
             moviesMap.put("screens", screensList);
             timeTablesList.add(moviesMap);
         }
  
         Map<String, Object> returnMap = new HashMap<String, Object>();
         returnMap.put("showtimes", new Gson().fromJson(new Gson().toJson(timeTablesList), timeTablesList.getClass()));
+
+        Long end = System.currentTimeMillis();
+        sum += end-start;
+        say ++;
+        
+        if(say == 5){
+            System.out.println(sum);
+            sum = 0;
+            say = 0;
+        }
+        
 
         return returnMap;
     }//end of getTimeTablesByCinemaId
